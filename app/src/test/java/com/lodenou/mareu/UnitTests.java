3dda73831b3a8b00e594363226e0a434de0881d6
lodenou <denoual.loic.dl@gmail.com>
2020-05-15 20:57:31 +0200

    Attempts on UnitTests

--- app/src/test/java/com/lodenou/mareu/UnitTests.java
+++ app/src/test/java/com/lodenou/mareu/UnitTests.java
@@ -4,14 +4,24 @@ import android.os.Build;
 
 import androidx.annotation.RequiresApi;
 
+import com.lodenou.mareu.controller.ActivityListMareu;
 import com.lodenou.mareu.controller.DummyReunionApiService;
+import com.lodenou.mareu.di.DI;
 import com.lodenou.mareu.model.Reunion;
 import com.lodenou.mareu.view.ReunionAdapter;
 
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Spy;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Locale;
 
 import static org.junit.Assert.*;
 
@@ -25,6 +35,7 @@ public class UnitTests {
     private DummyReunionApiService mService;
     private Reunion mReunion;
     private ReunionAdapter mAdapter;
+    ActivityListMareu activityListMareu;
 
     @Test
     public void addMeetingWithSuccess() {
@@ -46,27 +57,44 @@ public class UnitTests {
     }
 
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+
     @Test
     public void hourFilterWorks() {
         List<Reunion> mListReunion1 = new ArrayList<>();
-
-
+//        List<Reunion> mListReunion1 = DI.getNeighbourApiService().getmReunions();
         Reunion mReunion0;
         Reunion mReunion1;
         Reunion mReunion2;
-        mListReunion1.add(mReunion0 = new Reunion("Blablabla", "08h20", "Daisy", "hhhd@gg.dd"));
-        mListReunion1.add(mReunion1 = new Reunion("Blablabla", "09h20", "Daisy", "hhhd@gg.dd"));
-        mListReunion1.add(mReunion2 = new Reunion("Blablabla", "08h20", "Daisy", "hhhd@gg.dd"));
 
 
-        mListReunion1.removeIf(mReunion -> (!mReunion.getTimeReu().contains("08h")));
 
+        Calendar calendar0 = Calendar.getInstance();
+        calendar0.set(2020, 5, 13, 9, 59, 59);
+        Date date0 = calendar0.getTime();
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.set(2020, 5, 13, 9, 59, 59);
+        Date date1 = calendar1.getTime();
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.set(2020, 5, 13, 11, 59, 59);
+        Date date2 = calendar2.getTime();
+
+        mListReunion1.add(mReunion0 = new Reunion("Blablabla", date0 , "Daisy", "hhhd@gg.dd"));
+        mListReunion1.add(mReunion1 = new Reunion("Blablabla", date1, "Daisy", "hhhd@gg.dd"));
+        mListReunion1.add(mReunion2 = new Reunion("Blablabla", date2, "Daisy", "hhhd@gg.dd"));
+        mReunion0 = mAdapter.verifFilter().get(0);
+        mReunion1 = mAdapter.verifFilter().get(1);
+        mReunion2 = mAdapter.verifFilter().get(2);
+
+
+        // test
+            mListReunion1.remove(mReunion2);
+
+//            activityListMareu.filterPerHour(date0);
+        mAdapter.setFilterDate(date0);
 
         assertTrue(mListReunion1.contains(mReunion0));
-        assertTrue(mListReunion1.contains(mReunion2));
-        assertFalse(mListReunion1.contains(mReunion1));
-
+        assertTrue(mListReunion1.contains(mReunion1));
+        assertFalse(mListReunion1.contains(mReunion2));
     }
 
     @Test
@@ -75,13 +103,26 @@ public class UnitTests {
         Reunion mReunion0;
         Reunion mReunion1;
         Reunion mReunion2;
-        mListReunion1.add(mReunion0 = new Reunion("Blablabla", "08h20", "Bowser", "hhhd@gg.dd"));
-        mListReunion1.add(mReunion1 = new Reunion("Blablabla", "09h20", "Daisy", "hhhd@gg.dd"));
-        mListReunion1.add(mReunion2 = new Reunion("Blablabla", "08h20", "Bowser", "hhhd@gg.dd"));
-
-
-        mListReunion1.removeIf(mReunion -> (!mReunion.getRoomReu().contains("Bowser")));
-
+        Calendar calendar0 = Calendar.getInstance();
+        calendar0.set(2020, 5, 13, 9, 59, 59);
+        Date date0 = calendar0.getTime();
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.set(2020, 5, 13, 9, 59, 59);
+        Date date1 = calendar1.getTime();
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.set(2020, 5, 13, 11, 59, 59);
+        Date date2 = calendar2.getTime();
+//        mListReunion1.add(mReunion0 = new Reunion("Blablabla", date0, "Bowser", "hhhd@gg.dd"));
+//        mListReunion1.add(mReunion1 = new Reunion("Blablabla", date1, "Daisy", "hhhd@gg.dd"));
+//        mListReunion1.add(mReunion2 = new Reunion("Blablabla", date2, "Bowser", "hhhd@gg.dd"));
+//        mListReunion1.removeIf(mReunion -> (!mReunion.getRoomReu().contains("Bowser")));
+        mReunion0 = mAdapter.verifFilter().set(0, new Reunion("Blablabla", date0, "Bowser", "hhhd@gg.dd"));
+        mReunion1 = mAdapter.verifFilter().set(1, new Reunion("Blablabla", date1, "Daisy", "hhhd@gg.dd"));
+        mReunion2 = mAdapter.verifFilter().set(2, new Reunion("Blablabla", date2, "Bowser", "hhhd@gg.dd"));
+        mListReunion1.add(mReunion0);
+        mListReunion1.add(mReunion1);
+        mListReunion1.add(mReunion2);
+        mAdapter.setFilterRoom("Bowser");
         assertTrue(mListReunion1.contains(mReunion0));
         assertTrue(mListReunion1.contains(mReunion2));
         assertFalse(mListReunion1.contains(mReunion1));
